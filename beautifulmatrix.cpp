b484cf6c0c13df98a3f6c41a752d5c6f6bb6dd3c
Amina Islam Katha <140314069+aminakatha@users.noreply.github.com>
2023-08-20 14:42:02 +0600

    Add files via upload

---
+++ beautifulmatrix.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int v;
+    for(int i=1; 1<=5; i++)
+    {
+        for(int j=1; 1<=5; j++)
+    {
+
+
+        cin>>v;
+        if(v == 1)
+            {
+                cout << abs(3-i) +  abs(3-j) << endl;
+            }
+        }
+    }
+    return 0;
+}
