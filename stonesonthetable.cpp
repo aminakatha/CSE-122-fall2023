ce98bdd9da0413ea02c59874944e820b9d8f9471
Amina Islam Katha <140314069+aminakatha@users.noreply.github.com>
2023-09-24 11:11:12 +0600

    Add files via upload

---
+++ stonesonthetable.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int minRemovals = 0;
+
+    for (int i = 1; i < n; ++i) {
+        if (s[i] == s[i - 1]) {
+            minRemovals++;
+        }
+    }
+
+    cout << minRemovals << endl;
+
+    return 0;
+}
+
